14a94f109dd41786625e4605292f959dda9b675a
Mark <365764028@qq.com>
2016-12-08 15:47:54 +0800

    Initial Commit

---
+++ Mark_CoreText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mark_CoreText
+//
+//  Created by Mark on 2016/12/8.
+//  Copyright © 2016年 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mark_CoreText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mark_CoreText
+//
+//  Created by Mark on 2016/12/8.
+//  Copyright © 2016年 Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
